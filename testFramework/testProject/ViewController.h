0092efebb8a1a5b2f50d1d9b4b1a90956c356bf3
gs01md <gshzyfx@163.com>
2016-04-11 16:33:55 +0800

    初始工程，添加测试工程
    用来引用framework及测试

---
+++ testFramework/testProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testProject
+//
+//  Created by Simon on 16/4/11.
+//  Copyright © 2016年 gaoshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testFramework/testProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testProject
+//
+//  Created by Simon on 16/4/11.
+//  Copyright © 2016年 gaoshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
