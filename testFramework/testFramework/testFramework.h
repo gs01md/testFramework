b7907db80777e7f3e756d65c3555eeccc4cafbaa
gs01md <gshzyfx@163.com>
2016-04-11 16:29:49 +0800

    初始工程

---
+++ testFramework/testFramework/testFramework.h
@@ -0,0 +1,19 @@
+//
+//  testFramework.h
+//  testFramework
+//
+//  Created by Simon on 16/4/11.
+//  Copyright © 2016年 gaoshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testFramework.
+FOUNDATION_EXPORT double testFrameworkVersionNumber;
+
+//! Project version string for testFramework.
+FOUNDATION_EXPORT const unsigned char testFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testFramework/PublicHeader.h>
+
+
